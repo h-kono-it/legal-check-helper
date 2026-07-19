5155dadbb830753f5afcb06ed7e80ad04acd4f33
h-kono-it <h.kono.it@gmail.com>
2026-07-19 16:46:26 +0900

    資産計上系の項目を追加

---
+++ docs/10-features/60-assets/meta.ts
@@ -0,0 +1,7 @@
+import { defineMeta } from "blume";
+
+export default defineMeta({
+  title: "開発費・資産系",
+  icon: "calculator",
+  pages: ["index", "capitalization", "impairment", "retirement"],
+});
--- docs/10-features/meta.ts
+++ docs/10-features/meta.ts
@@ -3,5 +3,5 @@ import { defineMeta } from "blume";
 export default defineMeta({
   title: "機能から引く",
   icon: "zap",
-  pages: ["index", "10-money", "20-privacy", "30-people", "40-content", "50-accounting"],
+  pages: ["index", "10-money", "20-privacy", "30-people", "40-content", "50-accounting", "60-assets"],
 });
