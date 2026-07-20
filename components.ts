21a1804df6fc0e609dfb25a534cf3f6f1e4e8e88
h-kono-it <h.kono.it@gmail.com>
2026-07-20 11:46:00 +0900

    share ボタン追加

--- components.ts
+++ components.ts
@@ -4,5 +4,8 @@ export default defineComponents({
   layout: {
     // 全ページ共通のフッター。免責事項を必ず表示する
     Footer: "./components/SiteFooter.astro",
+    // シェアメニュー。上（タイトル直上・逆引き動線用）と下（読了後用）の2箇所
+    PageHeader: "./components/ShareMenuTop.astro",
+    PageFooter: "./components/ShareMenu.astro",
   },
 });
